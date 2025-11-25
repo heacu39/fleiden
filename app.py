975cc84a9f1519504da8afe2413f8eb72a4cd40d
Edward Garrett <189803938+heacu39@users.noreply.github.com>
2025-11-25 20:14:32 +0100

    update with connection to redis

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import json
+import redis
+from redis.commands.search.query import Query
+
 app = Flask(__name__)
+application = app
+
+pool = redis.ConnectionPool(host='178.62.124.120', port=6379, db=0)
+#pool = redis.ConnectionPool(host='192.168.1.20', port=6379, db=0)
+red = redis.Redis(connection_pool=pool)
 
 @app.route('/')
 def hello_world():
