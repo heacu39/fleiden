b03ddc2adab783c72aff47f1b7ef6b94f25d9f23
Edward Garrett <189803938+heacu39@users.noreply.github.com>
2025-11-26 18:55:08 +0100

    try something

--- app.py
+++ app.py
@@ -8,23 +8,25 @@ application = app
 
 r = redis.Redis(host='178.62.124.120', port=6379)
 
-def parseJSON(doc):
-    doc.json = json.loads(doc.json)
-    return doc
+#def parseJSON(doc):
+#    doc.json = json.loads(doc.json)
+#    return doc
 
 @app.get("/search/<manifest>")
 def search_get(manifest):
     term = request.args.get('q', '')
     res = r.ft('jvm').search(Query(f"@manifest:{{{manifest}}} @value:{{{term}}}"))
-    docs = map(parseJSON, res.docs)
+    #docs = map(parseJSON, res.docs)
     return render_template("results.html", docs = docs)
-    #items = []
-    #for doc in docs:
-    #    items.append(doc.json.annotation)
-    #dict = {
-    #    "@context": "http://iiif.io/api/search/2/context.json",
-    #    "id": "url",
-    #    "type": "AnnotationPage",
-    #    "items": items
-    #}        
-    #return jsonify(dict)
+    items = []
+    for doc in res.docs:
+        container = json.loads(doc.json)
+        items.append(container.annotation)
+    
+    dict = {
+        "@context": "http://iiif.io/api/search/2/context.json",
+        "id": "url",
+        "type": "AnnotationPage",
+        "items": items
+    }        
+    return jsonify(dict)
