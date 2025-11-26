be5fd537a5bd6e97daac7b9295e82eb53a9e9e54
Edward Garrett <189803938+heacu39@users.noreply.github.com>
2025-11-26 16:47:07 +0100

    try something

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def hello_world():
 @app.get("/search")
 def search_get():
     term = request.args.get('q', '')
-    res = r.ft('jvm').search(Query(f"@value:{term}"))
+    res = r.ft('jvm').search(Query("@value:ᰠ"))
     docs = map(parseJSON, res.docs)
     #return docs
-    return render_template('results.html', total=res.total, term=term, docs=docs)
+    return render_template('results.html', docs=docs)
