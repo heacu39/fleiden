b0a548b38503e784fc98c0af84ab1afb2f47b796
Edward Garrett <189803938+heacu39@users.noreply.github.com>
2025-11-28 01:06:33 +0100

    test page search with slop

--- app.py
+++ app.py
@@ -8,6 +8,15 @@ application = app
 
 r = redis.Redis(host='178.62.124.120', port=6379)
 
+@app.get("/page/search")
+    term = request.args.get('q', '')
+    res = r.ft('jvm').search(Query(f"@page:{{{term}}} SLOP {1}"))
+    items = []
+    for doc in res.docs:
+        container = json.loads(doc.json)
+        items.append(container)
+    return jsonify(items)
+    
 @app.get("/search/<manifest>")
 def search_get(manifest):
     term = request.args.get('q', '')
